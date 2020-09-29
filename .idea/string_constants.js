18c6d351ab194e832c89702677d4c651501ec209
egoldshm <39948614+egoldshm@users.noreply.github.com>
2020-09-29 14:10:41 +0300

    added files and simple bot

---
+++ .idea/string_constants.js
@@ -0,0 +1,2 @@
+const SEND_THE_LAST_NEWS = "שלח את מהדורת החדשות האחרונה ⌛"
+const NOT_FOUND_MESSAGE = "לא הבנתי, נא לבחור אפשרות מהתפריט"
--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-fo
+const BOT_TOKEN = "1121484069:AAFxBW8E__DOTcQ8n0qIufiKg2s_kuhppIo"
+const { Telegraf } = require('telegraf')
+const Keyboard = require('telegraf-keyboard');
+
+
+const bot = new Telegraf(BOT_TOKEN)
+bot.start((ctx) => 
+{
+ctx.reply('Welcome!');
+})
+bot.on('text', (ctx) => 
+{
+    const keyboad = new Keyboard()
+    console.log(ctx.update)
+    if(ctx.update.message.text == SEND_THE_LAST_NEWS)
+    {
+        ctx.replyWithAudio("./audio.mp3").catch((reason) => console.log("ERROR:" + reason));
+    }
+    else
+    {
+        ctx.reply(NOT_FOUND_MESSAGE,keyboad.add(SEND_THE_LAST_NEWS).draw())
+    }
+}
+)
+bot.launch()
